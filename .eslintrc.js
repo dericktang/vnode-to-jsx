36ea27af61a53440dee6c3ad1cc8c076a33c619c
dericktang <57390305@qq.com>
2019-09-04 10:51:46 +0800

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "plugins": [
+        "flowtype"
+    ],
+    "extends": [
+        "eslint:recommended",
+        "plugin:flowtype/recommended"
+    ],
+    "parserOptions": {
+        parser: require.resolve('babel-eslint'),
+        ecmaVersion: 2018,
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
